4f49dec9d847888000d904a53b1cad74bc2c2806
atonx22 <137027382+atonx22@users.noreply.github.com>
2023-08-08 22:44:44 -0500

    Add files via upload

---
+++ src/main/test/java/com/mycompany/views/LoginTest.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package com.mycompany.views;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author maite
+ */
+public class LoginTest {
+    
+    public LoginTest() {
+    }
+
+    @org.junit.Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Login.main(args);
+        
+    }
+    
+}
